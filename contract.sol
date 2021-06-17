ed666306925e29f6d45a78883679efb561cf1b60
hskim0102 <actpenguin.kim@gmail.com>
2021-06-17 19:44:21 +0900

    202120616

--- contract.sol
+++ contract.sol
@@ -1,7 +1,5 @@
 pragma solidity ^0.4.26;
 
-//분할양도
-//기간 만료
 
 contract ContractPjt{
  
@@ -20,7 +18,7 @@ contract ContractPjt{
     }
  
     //계약 제목으로 계약서 맵핑
-    mapping(string => contractList) private ctList; 
+    mapping(string => contractList) private ctList;
     
     address public Owner;           //계약의 주인 주소
     
@@ -28,7 +26,7 @@ contract ContractPjt{
     
     // 오너가 아니면 수정을 못하게 한다.
     modifier onlyOwner{
-        require(msg.sender == Owner);
+        require(msg.sender == Owner, "Only Owner can call this.");
          _;
     }
     
@@ -37,8 +35,10 @@ contract ContractPjt{
     }
     
     //계약서 작성 
-    function setContract(string _title, string _content, uint _date, uint _validate_date, uint256 _deposit, bool _isValid, bool _isSecret, bool _isTransferable){
-        contractList storage ctrt;
+    function setContract(string _title, string _content, uint _date, uint _validate_date, uint256 _deposit, bool _isValid, bool _isSecret, bool _isTransferable) public {
+        require(Owner != msg.sender, "contractor is not same with Owner");
+        
+        var ctrt = ctList[_title];
         ctrt.contractor = msg.sender;
         ctrt.title = _title;
         ctrt.content = _content;
@@ -49,40 +49,56 @@ contract ContractPjt{
         ctrt.isSecret = _isSecret;
         ctrt.isTransferable = _isTransferable;
         ctrt.isChecked = false;             //계약자가 쓴 계약서를 아직 확인하지 않은 상태
-
-        ctList[_title] = ctrt;
     }
 
     //계약자의 계약서 확인
-    function checkContract(string title) public onlyOwner {
+    function checkContract(string title) payable public onlyOwner returns(uint256) {
         ctList[title].isChecked = true;
+        require(Owner.balance >= ctList[title].deposit);
+        require(msg.value == ctList[title].deposit * 1 ether, "It must be same with deposit");
         
         emit NewContract(ctList[title].contractor, ctList[title].title, ctList[title].content, ctList[title].date, ctList[title].validate_date, ctList[title].deposit, ctList[title].isValid, ctList[title].isSecret, ctList[title].isTransferable);
+        return ctList[title].deposit;
     }
 
     //계약서 내용 열람
     function showContent(string title) public returns(string) {
         if(!ctList[title].isSecret) {
             return ctList[title].content; 
-        } else if (msg.sender == Owner && msg.sender == ctList[title].contractor) {
+        } else if (msg.sender == Owner || msg.sender == ctList[title].contractor) {
             return ctList[title].content;
         }
 
     }
 
     //계약금 송금
-    function payContract(string title) public onlyOwner payable returns(uint256) {
-        contractList localCt = ctList[title];
-        require(localCt.isValid);
-        require(localCt.isChecked);
+    function payContract(string title) payable public onlyOwner {
+        contractList storage localCt = ctList[title];
+        require(localCt.isValid, "It is not a valid contract");
+        require(localCt.isChecked, "It is not confirmed by owner");
         
-        require(Owner.balance >= localCt.deposit);
-
+        localCt.deposit = localCt.deposit * 1 ether;
+        
         localCt.contractor.transfer(localCt.deposit);
     }
     
+    //양도 요청
+    function requestTransferContract(string title, uint percent) payable public {
+        require(Owner != msg.sender, "request contractor is not same with Owner");
+        
+        contractList storage localCt = ctList[title];
+        uint remise = ctList[title].deposit * percent / 100;
+        ctList[title].deposit = remise;
+        require(msg.value == remise, "It must be same with remise");
+    }
+    
     //계약서 양도 ( 양도금 송금)
-    function transferContract(address contractor, string title) public onlyOwner {
-        delete ctList[title];
+    function transferContract(string title) public onlyOwner returns (uint){
+        contractList storage localCt = ctList[title];
+        Owner.transfer(localCt.deposit);
+        //delete ctList[title];
+        
+        return localCt.deposit;
+        
     }
 }
